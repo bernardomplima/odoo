a9721333a25fdcbf053c38c3220d178bb89b5820
bernardomplima <bernardomplima2005@gmail.com>
2025-05-08 09:32:34 +0000

    My first module

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -1,15 +1,16 @@
-from odoo import models, api, _
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     @api.constrains('product_qty', 'bom_id')
     def _check_bom_multiple(self):
-        for production in self:
-            bom = production.bom_id
-            if bom and bom.product_qty > 0:
-                if production.product_qty % bom.product_qty != 0:
-                    raise ValidationError(_(
-                        "A quantidade a produzir (%s) deve ser um múltiplo da quantidade definida na Lista de Materiais (%s)."
-                    ) % (production.product_qty, bom.product_qty))
+        for record in self:
+            if record.bom_id and record.bom_id.product_rounding:
+                multiple = record.bom_id.product_rounding
+                if multiple > 0 and record.product_qty % multiple != 0:
+                    raise ValidationError(
+                        f"A quantidade a produzir ({record.product_qty}) "
+                        f"deve ser múltipla de {multiple} definido na Lista de Materiais."
+                    )
