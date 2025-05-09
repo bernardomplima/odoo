d7232c7a3e4eb08a122150bf096addbf273d9284
bernardomplima <bernardomplima2005@gmail.com>
2025-05-09 09:06:00 +0000

    My first module

--- addons/my_module/__manifest__.py
+++ addons/my_module/__manifest__.py
@@ -1,34 +1,13 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "my_module",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'name': 'MRP BOM Rounding Constraint',
+    'version': '16.0.1.0.0',
+    'summary': 'Restringe ordens de produção a múltiplos definidos na BoM',
+    'description': 'Garante que a quantidade a produzir respeita o múltiplo da lista de materiais.',
+    'category': 'Manufacturing',
+    'author': 'Seu Nome',
+    'depends': ['mrp'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
 }
-
--- addons/my_module/models/models.py
+++ addons/my_module/models/models.py
@@ -1,19 +1,26 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import math
 
-# from odoo import models, fields, api
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
 
+    rounding = fields.Float(
+        string='Múltiplo de Produção',
+        default=1.0,
+        help="A quantidade a produzir deve ser múltipla deste valor."
+    )
 
-# class my_module(models.Model):
-#     _name = 'my_module.my_module'
-#     _description = 'my_module.my_module'
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    @api.constrains('product_qty')
+    def _check_qty_rounding(self):
+        for record in self:
+            if record.bom_id and record.bom_id.rounding > 0:
+                rounding = record.bom_id.rounding
+                qty = record.product_qty
+                if not math.isclose(qty % rounding, 0.0, abs_tol=1e-6):
+                    raise ValidationError(
+                        f"A quantidade a produzir ({qty}) deve ser múltipla de {rounding}."
+                    )
