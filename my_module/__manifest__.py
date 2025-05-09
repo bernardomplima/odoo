0594bc45824477c89c101cda18b555ac859e4784
bernardomplima <bernardomplima2005@gmail.com>
2025-05-09 08:47:27 +0000

    My first module

--- my_module/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Production Quantity Multiple Validator",
-    "version": "16.0.1.0.0",
-    "author": "Seu Nome",
-    "depends": ["mrp"],
-    "data": [],
-    "installable": True,
-    "application": True,
-}
